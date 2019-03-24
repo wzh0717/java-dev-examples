ecebb93aed4c7b981484da06b24b7107f4b5dfe3
wzh0717 <w-z-h06@163.com>
2019-03-24 19:14:34 +0800

    spring boot 2.1.3 init

---
+++ weixin-java-cp/src/main/java/com/wangzh/app/weixin/pa/WeixinJavaCpApplication.java
@@ -0,0 +1,13 @@
+package com.wangzh.app.weixin.pa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeixinJavaCpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeixinJavaCpApplication.class, args);
+	}
+
+}
---
+++ weixin-java-cp/src/test/java/com/wangzh/app/weixin/pa/WeixinJavaCpApplicationTests.java
@@ -0,0 +1,16 @@
+package com.wangzh.app.weixin.pa;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WeixinJavaCpApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
