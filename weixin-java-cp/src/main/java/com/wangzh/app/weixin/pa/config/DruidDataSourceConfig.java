e4e2efc39dc4879af4a4a3bcaee7f0a0af2899df
wzh0717 <w-z-h06@163.com>
2019-03-25 15:29:32 +0800

    druidDataSource

---
+++ weixin-java-cp/src/main/java/com/wangzh/app/weixin/pa/config/DruidDataSourceConfig.java
@@ -0,0 +1,64 @@
+package com.wangzh.app.weixin.pa.config;
+
+import com.alibaba.druid.pool.DruidDataSource;
+import com.alibaba.druid.support.http.StatViewServlet;
+import com.alibaba.druid.support.http.WebStatFilter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+/**
+ * @Description:
+ * @CreatedDate:2019-03-25 14:30
+ * @Author:wangzh
+ */
+@Configuration
+public class DruidDataSourceConfig {
+
+    @Bean
+    @ConfigurationProperties(prefix = "spring.datasource")
+    public DataSource druidDataSource() {
+        DruidDataSource druidDataSource = new DruidDataSource();
+        return druidDataSource;
+    }
+
+    /**
+     * 注册Servlet信息， 配置监控视图
+     *
+     * @return
+     */
+    @Bean
+    public ServletRegistrationBean druidServlet() {
+        ServletRegistrationBean servletRegistrationBean = new ServletRegistrationBean(new StatViewServlet(), "/druid/*");
+        //白名单：
+        servletRegistrationBean.addInitParameter("allow", "127.0.0.1");
+        //IP黑名单 (共存时，deny优先于allow) : 如果满足deny的话提示:Sorry, you are not permitted to view this page.
+        servletRegistrationBean.addInitParameter("deny", "192.168.6.73");
+        //登录查看信息的账号密码, 用于登录Druid监控后台
+        servletRegistrationBean.addInitParameter("loginUsername", "admin");
+        servletRegistrationBean.addInitParameter("loginPassword", "123456a");
+        //是否能够重置数据.
+        servletRegistrationBean.addInitParameter("resetEnable", "false");
+        return servletRegistrationBean;
+    }
+
+    /**
+     * 配置web监控filter
+     *
+     * @return
+     */
+    @Bean
+    public FilterRegistrationBean filterRegistrationBean() {
+        FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean();
+        filterRegistrationBean.setFilter(new WebStatFilter());
+        //添加过滤规则
+        filterRegistrationBean.addUrlPatterns("/*");
+        //添加忽略的格式信息
+        filterRegistrationBean.addInitParameter("exclusions", "*.js,*.gif,*.jpg,*.png,*.css,*.ico,/druid/*");
+        return filterRegistrationBean;
+    }
+}
---
+++ weixin-java-cp/src/main/java/com/wangzh/app/weixin/pa/config/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * @Description:
+ * @CreatedDate:2019-03-25 14:09
+ * @Author:wangzh
+ */
+package com.wangzh.app.weixin.pa.config;
